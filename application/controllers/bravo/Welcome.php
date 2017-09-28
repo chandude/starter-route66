98e1d6cbadf7c88f53478fec36382f6799837e95
Chandu <chandu.dissanayake3@gmail.com>
2017-09-28 14:53:05 -0700

    Issue Bravo resolved

---
+++ application/controllers/bravo/Welcome.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chand
+ * Date: 2017-09-28
+ * Time: 2:44 PM
+ */
+
+class Welcome extends Application
+
+{
+
+    public function index() {
+        $this->show(2);
+    }
+}
